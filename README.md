e56f69c369d53ec27fd81dcf18f5b152b638e09b
NicolasRivera <nico.acevedo.nr@gmail.com>
2020-08-09 23:34:57 -0400

    GoogleSign
    -Se agregan librerias para front y back . estas para resolver y verificar los token de autenticacion.
    -Se agrega creacion de usuario una vez autenticado
    -Control de errores.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 node-rest-server
+https://documenter.getpostman.com/view/4547750/T1LJm92B?version=latest
--- server/server.js
+++ server/server.js
@@ -3,13 +3,18 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
-
+const path = require('path')
 
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
     // parse application/json
 app.use(bodyParser.json())
+
+//habilitar carpeta publica
+
+app.use(express.static(path.resolve(__dirname , '../public')));
+console.log(path.resolve(__dirname , '../public'));
 
 //Configuracion de rutas globales 
 app.use(require('./routes/index'))
